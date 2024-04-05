ddaf7f51bbfc674e89e8eed107dfecd06de42536
Owensaug <brunowagnersoares@mgial.com>
2024-04-05 13:23:44 -0300

    Initial commit

---
+++ src/main/java/net/owensaug/modteste/ModTesteModClient.java
@@ -0,0 +1,10 @@
+package net.owensaug.modteste;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTesteModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
